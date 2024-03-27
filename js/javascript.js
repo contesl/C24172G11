117eaba04de4d15351917a862573d19ef0722abd
sconte <contesl@gmail.com>
2024-03-27 13:47:47 -0300

    incorporacion de js externo

---
+++ js/javascript.js
@@ -0,0 +1,12 @@
+function confirmExit() {
+    window.location = '/index.html';
+}
+function ScrollMarquee() {
+
+    window.setTimeout('ScrollMarquee()', ScrollSpeed);
+
+    var msg = document.Cart_form.Message.value;
+
+    document.Cart_form.Message.value = msg.substring(ScrollChars) + msg.substring(0, ScrollChars);
+
+}
